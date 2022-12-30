11e39f313fc90aaf0486855c7b2b15799dc5d093
Franco Neil Glovasa <franconeilglovasa@outlook.com>
2022-12-30 15:43:18 +0800

    Add project files.

---
+++ WebAppTest1/Controllers/Api/DataController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace WebAppTest1.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataController : ControllerBase
+    {
+        private string _baseUrl;
+
+        public DataController()
+        {
+            _baseUrl = "https://localhost:44361/";
+        }
+
+
+        [HttpPut("UploadPic")]
+        public bool UploadPic(string basee64pic)
+        {
+
+            //make a function that save pic here
+
+            return true;
+        }
+
+        [HttpGet("GetVideoUrl")]
+        public string GetVideoUrl()
+        {
+
+            return $@"{_baseUrl}/video/videotest1.mp4";
+        }
+
+
+
+    }
+}
